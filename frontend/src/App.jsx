aa74591087602c0bca5ddcb858414f39dcc6aafe
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-20 00:02:56 -0700

    fix: correct component uploads in main app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
+import ResumeUpload from "./Components/ResumeUpload/ResumeUpload";
+import JobUpload from "./Components/JobUpload/JobUpload";
+import Analysis from "./Components/Analysis/Analysis";
 
 function App() {
   const [resumeUploaded, setResumeUploaded] = useState(false);
