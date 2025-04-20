eda86cbea947cb5aa79184bcaf212e510499e140
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-19 23:35:58 -0700

    feat: implement App structural draft

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,24 @@ import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 
 function App() {
+  const [resumeUploaded, setResumeUploaded] = useState(false);
+  const [jobUploaded, setJobUploaded] = useState(false);
+  const [showResults, setShowResults] = useState(false);
+
   return (
-    <>
+    <div className="app-container">
       <Toaster />
-    </>
+      <ResumeUpload uploadHandler={() => setResumeUploaded(true)} />
+      <JobUpload uploadHandler={() => setJobUploaded(true)} />
+      <button
+        className="analyze-button"
+        disabled={!(resumeUploaded && jobUploaded)}
+        onClick={() => setShowResults(true)}
+      >
+        Analyze
+      </button>
+      {showResults && <Analysis />}
+    </div>
   );
 }
 
