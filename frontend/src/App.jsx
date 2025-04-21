93a2762eef46076da0fbce03139cbe343d540f84
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-04-20 21:54:39 -0700

    feat: finish core implementation of upload handler

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,55 @@
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 import ResumeUpload from "./Components/ResumeUpload/ResumeUpload";
 import JobUpload from "./Components/JobUpload/JobUpload";
 import Analysis from "./Components/Analysis/Analysis";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 function App() {
   const [resumeUploaded, setResumeUploaded] = useState(false);
+  const [resume, setResume] = useState({
+    filename: "",
+    content: ""
+  });
   const [jobUploaded, setJobUploaded] = useState(false);
+  const [job, setJob] = useState({
+    filename: "",
+    content: ""
+  });
   const [showResults, setShowResults] = useState(false);
 
-  const handleUpload = async (file, type, onSuccess) => {
+  const uploadHandler = async (file, type, uploadedStateSetter, contentStateSetter) => {
     const formData = new FormData();
     formData.append(type, file);
 
-    await fetch(`http://localhost:8000/upload-${type}`, {
-      method: "POST",
-      body: formData,
-    });
-
-    onSuccess();
+    try {
+      const api_response = await fetch(`${apiUrl}/upload-${type}`, {
+        method: "POST",
+        body: formData,
+      });
+      toast.dismiss();
+      if (api_response.ok) {
+        const uploaded_object = await api_response.json();
+        uploadedStateSetter(true);
+        contentStateSetter(uploaded_object);
+        toast.success(`${type} uploaded successfully!`);
+      } else {
+        const error = await api_response.json();
+        console.log(error);
+        toast.error(`${type} upload failed.`);
+      }
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
     <div className="app-container">
       <Toaster />
-      <ResumeUpload uploadHandler={(file) => handleUpload(file, "resume", () => setResumeUploaded(true))} />
-      <JobUpload uploadHandler={(file) => handleUpload(file, "job", () => setJobUploaded(true))} />
+      <ResumeUpload uploadHandler={(file) => uploadHandler(file, "resume", setResumeUploaded, setResume)} />
+      <JobUpload uploadHandler={(file) => uploadHandler(file, "job", setJobUploaded, setJob)} />
       <button
         className="analyze-button"
         disabled={!(resumeUploaded && jobUploaded)}
