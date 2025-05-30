0bd7283d6a9e8ab6639fce12e7005a8c87e0a259
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-05-29 21:13:09 -0700

    chore: create script for local development environment setup

---
+++ scripts/local-dev-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo ""
+echo "📦 Installing pre-commit hook dependencies..."
+npm install
+echo "✅ Done."
+
+echo ""
+echo "📦 Installing frontend dependencies..."
+cd frontend
+npm install
+cd ../
+echo "✅ Done."
+
+echo ""
+echo "🐍 Setting up and activating Python virtual environment..."
+[ ! -d ".venv" ] && python3 -m venv .venv
+source .venv/bin/activate
+echo "✅ Done."
+
+echo ""
+echo "⬆️  Upgrading pip and installing backend dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+echo "✅ Done."
+
+echo ""
+echo "🎉 Setup complete."
+echo ""
